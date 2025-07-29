29471f46e3494b7005a593fff6dbd9a660ea66bd
woopzz <bkovtunovych@gmail.com>
2025-07-29 19:51:54 +0300

    Learn how to read the existing store from a file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#[derive(serde::Deserialize, Debug)]
+struct Store {
+    sessions: Vec<Session>,
+    labels: Vec<String>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Session {
+    id: String,
+    start_at: i64,
+    end_at: Option<i64>,
+    note: Option<String>,
+    labels: Vec<String>,
+}
+
+fn load_store<P: AsRef<std::path::Path> + std::fmt::Display>(path: P) -> Store {
+    let file = std::fs::File::open(&path).expect(&format!("Could not open the database file \"{}\".", &path));
+    let reader = std::io::BufReader::new(file);
+    let store: Store = serde_json::from_reader(reader).expect("Could not parse the database file as a JSON file.");
+    return store;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path_to_store_file = std::env::var("WTT_PATH_DATABASE").unwrap_or("db.json".to_string());
+    let store = load_store(path_to_store_file);
+    println!("{:?}", store);
 }
